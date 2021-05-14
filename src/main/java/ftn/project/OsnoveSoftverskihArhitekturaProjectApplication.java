a0646c2c936d2b59ea54dbe124629621782ae6f1
wlqmpek <milosvlku@hotmail.com>
2021-05-14 20:48:48 +0200

    initial commit

---
+++ src/main/java/ftn/project/OsnoveSoftverskihArhitekturaProjectApplication.java
@@ -0,0 +1,13 @@
+package ftn.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsnoveSoftverskihArhitekturaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsnoveSoftverskihArhitekturaProjectApplication.class, args);
+    }
+
+}
