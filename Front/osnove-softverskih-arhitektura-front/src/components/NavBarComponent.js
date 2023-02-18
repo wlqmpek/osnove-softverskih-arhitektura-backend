7bf48d49695fcb39275b8007d2b057585f3d62de
wlqmpek <wlqmpek@hotmail.com>
2023-02-18 13:03:05 +0100

    Hello

--- Front/osnove-softverskih-arhitektura-front/src/components/NavBarComponent.js
+++ Front/osnove-softverskih-arhitektura-front/src/components/NavBarComponent.js
@@ -1,6 +1,21 @@
 import {Navbar, Nav, Button, FormControl, Form, NavDropdown} from "react-bootstrap";
+import { AuthenticationService } from "../services/AuthenticationService";
+import React, { useState, useEffect } from "react";
+import * as Constants from "../helper/Constants";
+
 
 const NavBarComponent = () => {
+
+    const [userId, setUserId] = useState();
+    const [userRole, setUserRole] = useState();
+
+    const loggedIn = false;
+
+    useEffect(() => {
+        setUserId(AuthenticationService.getUserId());
+        setUserRole(AuthenticationService.getRole());
+    })
+
     return(
         <>
             <Navbar bg="dark" variant="dark">
@@ -9,17 +24,28 @@ const NavBarComponent = () => {
                     <Nav.Link href="/sellers">List</Nav.Link>
                     <Nav.Link href="/undelivered_orders">Undelivered</Nav.Link>
                     <Nav.Link href="/delivered_orders">Delivered</Nav.Link>
-                    <NavDropdown title="Seller" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="/my_articles">My Articles</NavDropdown.Item>
-                        <NavDropdown.Item href="/my_comments">Comment Managment</NavDropdown.Item>
-                        <NavDropdown.Item href="/create_discount">Create Discount</NavDropdown.Item>
-                        <NavDropdown.Item href="/create_article">Create Article</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="/login">Login</Nav.Link>
-                    <Nav.Link href="/logout">Logout</Nav.Link>
+                    {
+                        userRole === Constants.UserRoles.SELLER ? (
+                            <NavDropdown title="Seller" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="/my_articles">My Articles</NavDropdown.Item>
+                                <NavDropdown.Item href="/my_comments">Comment Managment</NavDropdown.Item>
+                                <NavDropdown.Item href="/create_discount">Create Discount</NavDropdown.Item>
+                                <NavDropdown.Item href="/create_article">Create Article</NavDropdown.Item>
+                            </NavDropdown>
+                        ) : null
+                    }
+                    {
+                        userId ? (
+                            <Nav.Link href="/logout">Logout</Nav.Link>
+                        ) : (
+                            <Nav.Link href="/login">Login</Nav.Link>
+                        )
+                    }
+
                 </Nav>
             </Navbar>
             <br />
+            <button onClick={() => {console.log(userRole === Constants.UserRoles.SELLER)}}>User Role</button>
 
         </>
     );
